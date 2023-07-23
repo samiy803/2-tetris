fba64ef5a9e92382ff7c89ce01006a6f6c89043a
Sami Yousef <mail@samiyousef.ca>
2023-07-23 17:18:27 -0400

    Ignore object files and executables

--- main.d
+++
@@ -1,5 +0,0 @@
-main.o: main.cc game.h player.h board.h blocks/block.h blocks/position.h \
-  factory/blockFactory.h xwindow.h /opt/X11/include/X11/Xlib.h \
-  /opt/X11/include/X11/X.h /opt/X11/include/X11/Xfuncproto.h \
-  /opt/X11/include/X11/Xosdefs.h /opt/X11/include/X11/Xutil.h \
-  /opt/X11/include/X11/keysym.h /opt/X11/include/X11/keysymdef.h
